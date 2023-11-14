4e2acda135d4d9bf39d9efee1378c60a19030472
neopragma <davenicolette@gmail.com>
2023-11-14 13:58:42 -0700

    Started Gilded Rose

--- tdd-gildedrose/src/main/java/com/gildedrose/GildedRose.java
+++ tdd-gildedrose/src/main/java/com/gildedrose/GildedRose.java
@@ -9,26 +9,18 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+            if (condition1(items[i])) {
+                items[i].quality--;
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -58,5 +50,13 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean condition1(Item item){
+        return ( (!item.name.equals("Aged Brie")
+        && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+        && (item.quality > 0)
+        && (!item.name.equals("Sulfuras, Hand of Ragnaros")) );
+
     }
 }
